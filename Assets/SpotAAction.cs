4e4b91352660ec9cd47088546b250dfa318c015a
AartHauk <andy111111111111111@gmail.com>
2025-10-25 04:43:00 -0600

    Messing With AI Behaviors

---
+++ Assets/SpotAAction.cs
@@ -0,0 +1,26 @@
+using System;
+using Unity.Behavior;
+using UnityEngine;
+using Action = Unity.Behavior.Action;
+using Unity.Properties;
+
+[Serializable, GeneratePropertyBag]
+[NodeDescription(name: "Spot A", story: "Go To A", category: "Action/Navigation", id: "c0d1a559c900edf64a26c1084fd0c7ba")]
+public partial class SpotAAction : Action
+{
+
+    protected override Status OnStart()
+    {
+        return Status.Running;
+    }
+
+    protected override Status OnUpdate()
+    {
+        return Status.Success;
+    }
+
+    protected override void OnEnd()
+    {
+    }
+}
+
